983c39efb5e66da9dc0e3e85e90299b0b603ce41
Tamar <tamar75262@gmail.com>
2025-09-28 12:30:45 +0300

    first part

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const bookRouter = require('./bookRouter')
+
+const app = express();
+app.use(express.json());
+
+app.use('/books', bookRouter)
+
+app.get('/', (req, res) => {
+    res.send("hello");
+})
+
+const PORT = 3000;
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+})
