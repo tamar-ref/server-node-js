25fa41299db0e9b1ceb18d03d09b5f729e913c6c
Tamar <tamar75262@gmail.com>
2025-09-30 22:18:45 +0300

    parts 3 4 5

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const cors = require('cors');
-const bookRouter = require('./bookRouter')
+const bookRouter = require('./routes/bookRouter')
+const userRouter = require('./routes/userRouter')
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 
 app.use('/books', bookRouter)
+app.use('/users', userRouter)
 
 app.get('/', (req, res) => {
     res.send("hello!");
